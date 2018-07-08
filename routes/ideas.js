d7182431925a9adceccec02d87648ce0831793d5
Mario Quintana <pyrros2000@gmail.com>
2018-07-08 01:34:30 -0700

    protecting routes that only users can navigate. None-users will be redirected to login page

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const router = express.Router();
 const mongoose = require('mongoose');
+const router = express.Router();
+const { ensureAuthenticated } = require('../helpers/auth');
 
 //Load idea Model
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
 
 //Idea Index page
-router.get('/', (req, res) => {
+router.get('/', ensureAuthenticated, (req, res) => {
     Idea.find({})
         .sort({date: 'desc'})
         .then(ideas => {
@@ -18,12 +19,12 @@ router.get('/', (req, res) => {
 });
 
 //Add Idea Form
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('ideas/add');
 });
 
 //Edit Idea Form
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
         _id: req.params.id
     })
@@ -35,7 +36,7 @@ router.get('/edit/:id', (req, res) => {
 });
 
 //Process form
-router.post('/', (req, res) => {
+router.post('/', ensureAuthenticated, (req, res) => {
     let errors = [];
 
     if(!req.body.title){
@@ -64,7 +65,7 @@ router.post('/', (req, res) => {
 });
 
 //Edit form process
-router.put('/:id', (req, res) => {
+router.put('/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
         _id: req.params.id
     })
@@ -82,7 +83,7 @@ router.put('/:id', (req, res) => {
 });
 
 //Delete idea
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
     Idea.remove({_id: req.params.id})
         .then(() => {
             req.flash('success_msg', 'Video idea removed')
