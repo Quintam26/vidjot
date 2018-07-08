b051eacd47e14dc8e9814b82c61c8a0212391587
Mario Quintana <pyrros2000@gmail.com>
2018-07-08 02:26:32 -0700

    testing

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://Mario:Mario27@ds129811.mlab.com:29811/vidjot-prod-mq'}
+    module.exports = {mongoURI: 'mongodb://Mario:mario27@ds129811.mlab.com:29811/vidjot-prod-mq'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
