94d121348c604a145f5f2be43fe9bb8332d4cec6
Mario Quintana <pyrros2000@gmail.com>
2018-07-08 02:08:07 -0700

    starting to use mlab for remote database, set up config database to specificy when to use mongoDB local database and mblab remote database

---
+++ config/database.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {mongoURI: 'mongodb://Mario:Mario27@ds129811.mlab.com:29811/vidjot-prod-mq'}
+} else {
+    module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
+}
